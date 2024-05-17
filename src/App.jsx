c9832c024b6be9102ee0b92ef2b6f807c354584b
Maziyar Amini <mzyramini@gmail.com>
2024-05-18 01:51:27 +0330

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./index.css"
+
+const App = () => {
+  return (
+    <div className="w-screen h-screen bg-gray-900">
+      
+    </div>
+  )
+}
+
+export default App;
