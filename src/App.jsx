4984b84a7b035b465a708dc6fbd6217df8fb9f9e
Maziyar Amini <mzyramini@gmail.com>
2024-05-19 00:20:26 +0330

    Implemented year wheel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import "./index.css"
+import YearWheel from './components/YearWheel.jsx';
 
 const App = () => {
   return (
     <div className="w-screen h-screen bg-gray-900">
-      
+      <YearWheel />
     </div>
   )
 }
