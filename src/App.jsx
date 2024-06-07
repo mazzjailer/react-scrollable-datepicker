07017d7cecfa7d2b531a8a1faa1d61033c2689c2
Maziyar Amini <mzyramini@gmail.com>
2024-06-07 05:07:03 +0330

    Moved App.jsx styles from styles.css to index.css, removed the public folder (Vite icon)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {useState} from 'react'
-import "./App.css"
+import './index.css'
 import DatePicker from './components/DatePicker.jsx';
 const App = () => {
   const [date, setDate] = useState();
